9ce9652ee429994456e2348b809196b35a5a5e3a
mturpin1 <44588255+mturpin1@users.noreply.github.com>
2023-06-03 17:44:27 -0700

    Add files via upload

--- comparison_graphic/script.js
+++ comparison_graphic/script.js
@@ -1,128 +1,94 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var body = document.querySelector("body");
-  var hoverableElements = document.querySelectorAll(".grid-cell-container");
-  const selectors = document.querySelectorAll(".selector");
-  const paragraphs = document.querySelectorAll(".grid-cell-paragraph");
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+  const hoverableElements = Array.from(document.querySelectorAll(".grid-container"));
+  const selectors = Array.from(document.querySelectorAll(".selector"));
+  const paragraphs = Array.from(document.querySelectorAll(".paragraph"));
 
-  hoverableElements.forEach(function (element) {
-    const gridCellLink = element.querySelector(".grid-cell-link");
-    gridCellLink.addEventListener("mouseover", function () {
+  let prevBackgroundImage = null;
+
+  hoverableElements.forEach((element) => {
+    const gridCellLink = element.querySelector('.link');
+
+    gridCellLink.addEventListener("mouseover", () => {
+      prevBackgroundImage = body.style.getPropertyValue("--background-image");
+
+      let backgroundImage;
+
       if (element.id == "aip-vid-cell") {
-        var aipVidParagraphId = null;
-        const aipVidCell = document.getElementById("aip-vid-cell");
-        const aipVidParagraphs = aipVidCell.querySelectorAll(
-          ".grid-cell-paragraph"
-        );
-        const aipVidParagraphsArray = Array.from(aipVidParagraphs);
-        const activeAipVidParagraphs = aipVidParagraphsArray.filter(
-          (aipVidParagraph) => aipVidParagraph.classList.contains("active")
-        );
-        activeAipVidParagraphs.forEach((paragraph) => {
-          aipVidParagraphId = paragraph.id;
-        });
-        if (aipVidParagraphId == "use-case") {
-          body.style.setProperty(
-            "--background-image",
-            "url('assets/aip-vid_v1.svg')"
-          );
-        } else if (aipVidParagraphId == "tech") {
-          body.style.setProperty(
-            "--background-image",
-            "url('assets/diagram-export-5_29_2023, 9_34_26 AM.svg')"
-          );
-        } else if (aipVidParagraphId == "interface") {
-          body.style.setProperty(
-            "--background-image",
-            "url('assets/aip-vid_v1.svg')"
-          );
-        } else if (aipVidParagraphId == "privacy") {
-          body.style.setProperty(
-            "--background-image",
-            "url('assets/diagram-export-5_29_2023, 9_34_26 AM.svg')"
-          );
-        } else if (aipVidParagraphId == "integration") {
-          body.style.setProperty(
-            "--background-image",
-            "url('assets/aip-vid_v1.svg')"
-          );
-        } else if (aipVidParagraphId == "ease") {
-          body.style.setProperty(
-            "--background-image",
-            "url('assets/flowchart.png')"
-          );
-        } else if (aipVidParagraphId == "accessibility") {
-          body.style.setProperty(
-            "--background-image",
-            "url('assets/flowchart.png')"
-          );
-        } else if (aipVidParagraphId == "target-market") {
-          body.style.setProperty(
-            "--background-image",
-            "url('assets/aip-vid_v1.svg')"
-          );
+        const activeParagraph = element.querySelector(".paragraph.active");
+        if (activeParagraph) {
+          switch(activeParagraph.id) {
+            case "use-case":
+            case "interface":
+            case "integration":
+            case "target-market":
+                backgroundImage = "url('assets/aip-vid_v1.svg')";
+                break;
+            case "tech":
+            case "privacy":
+                backgroundImage = "url('assets/diagram-export-5_29_2023, 9_34_26 AM.svg')";
+                break;
+            case "ease":
+            case "accessibility":
+                backgroundImage = "url('assets/flowchart.png')";
+                break;
+            default:
+                backgroundImage = "url('assets/aip-vid_v1.svg')";
+          }
         } else {
-          body.style.setProperty(
-            "--background-image",
-            "url('assets/aip-vid_v1.svg')"
-          );
+          backgroundImage = "url('assets/aip-vid_v1.svg')";
         }
       } else if (element.id == "echospeech-cell") {
-        body.style.setProperty(
-          "--background-image",
-          "url('assets/echospeech_logo.jpg')"
-        );
+        backgroundImage = "url('assets/echospeech_logo.jpg')";
       } else if (element.id == "voiceitt-cell") {
-        body.style.setProperty(
-          "--background-image",
-          "url('assets/voiceitt_logo.webp')"
-        );
+        backgroundImage = "url('assets/voiceitt_logo.webp')";
       } else if (element.id == "nuance-communication-cell") {
-        body.style.setProperty(
-          "--background-image",
-          "url('assets/nuance_communications_logo.png')"
-        );
+        backgroundImage = "url('assets/nuance_communications_logo.png')";
       }
-      body.style.setProperty("--background-scale", "90%");
+
+      if (backgroundImage) {
+        body.style.setProperty("--background-image", backgroundImage);
+        body.style.setProperty("--background-scale", "90%");
+      }
     });
-    element.addEventListener("mouseout", function () {
+
+    element.addEventListener("mouseout", () => {
+      body.style.setProperty("--background-image", prevBackgroundImage);
       body.style.setProperty("--background-scale", "0");
     });
   });
 
   selectors.forEach((selector) => {
     selector.addEventListener("click", () => {
+      console.log(selector.id + " clicked");
+      selectors.forEach((otherSelector) => {
+        if (otherSelector !== selector) {
+          console.log(otherSelector.id + " active class removed")
+          otherSelector.classList.remove("active");
+        }
+      });
+
       if (selector.classList.contains('active')) {
-        selectors.forEach((selector) => {
-          selector.classList.remove("active");
-        });
+        selector.classList.remove('active');
       } else {
-        selectors.forEach((selector) => {
-          selector.classList.remove("active");
-        });
         selector.classList.add("active");
       }
+
       const selectorId = selector.getAttribute("id");
-      const correspondingParagraph = document.getElementById(
-        selectorId.replace("-selector", "")
-      );
-  
-      paragraphs.forEach((paragraph) => {
-        if ((paragraph.id == correspondingParagraph.id) && (!paragraph.classList.contains("active"))) {
-          paragraph.classList.add("active");
-          paragraph.style.maxHeight = paragraph.scrollHeight + "px";
-        } else {
+      const correspondingParagraphs = Array.from(document.querySelectorAll('#' + selectorId.replace('-selector', '')));
+
+      correspondingParagraphs.forEach((correspondingParagraph) => {
+        console.log(correspondingParagraph.id)
+        paragraphs.forEach((paragraph) => {
+          if (paragraph.id === correspondingParagraph.id && selector.classList.contains('active')) {
+            paragraph.classList.add("active");
+            paragraph.style.maxHeight = `${paragraph.scrollHeight}px`;
+          } else {
             paragraph.classList.remove("active");
-            correspondingParagraph.style.maxHeight = null;
+            paragraph.style.maxHeight = null;
           }
-      });
-  
-      // Move this check outside of the paragraphs.forEach loop
-      let content = correspondingParagraph.parentNode;
-      if (correspondingParagraph.classList.contains('active')) {
-        content.classList.add('child-active');
-      } else {
-        content.classList.remove('child-active');
-      }
+        });
+      })
     });
   });
 });
